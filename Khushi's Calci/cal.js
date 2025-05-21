d11cfbc40fa28dd846c902edbcc5b5bd212ec048
KhushiDivya <khushidivyaverma@gmail.com>
2025-05-22 02:06:35 +0530

    Calci

---
+++ Khushi's Calci/cal.js
@@ -0,0 +1,17 @@
+let dis=document.getElementById("display");
+
+function appendToDisplay(input){
+    dis.value+=input;
+}
+
+function clearDisplay(){
+    dis.value="";
+}
+
+function calculate(){
+    try {
+        dis.value=eval(dis.value);
+    } catch (error) {
+        dis.value="Error";
+    }
+}
